5c4192e9cd4f772f08ebc9814ab375f44161752c
imMatheus <matheusmendesbarata03@gmail.com>
2026-01-31 12:58:16 -0800

    added posthog analytics

---
+++ web/claude.md
@@ -0,0 +1,10 @@
+# Project Guidelines
+
+## Package Manager
+
+Always use **bun** for package management and running scripts:
+
+- Install packages: `bun add <package>`
+- Install dev dependencies: `bun add -d <package>`
+- Run scripts: `bun run <script>`
+- Execute files: `bun <file>`
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,17 +3,25 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { DataProvider } from "./contexts/DataContext";
+import { PostHogProvider } from 'posthog-js/react'
 import App from "./App.tsx";
 import "./index.css";
 
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <DataProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
-    </DataProvider>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <DataProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </DataProvider>
+    </PostHogProvider>
   </StrictMode>
 );
